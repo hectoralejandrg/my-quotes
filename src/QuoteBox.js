150a92540580e61c126114cf7f267d5c094efb41
hectoralejandrg <enero2169@gmail.com>
2021-05-02 22:48:10 -0500

    second commit

--- src/QuoteBox.js
+++ src/QuoteBox.js
@@ -1,18 +1,25 @@
 import { useState } from "react";
+import Colors from "./Colors";
 import ContainerButton from "./ContainerButton"
 import ContainerQuotes from "./ContainerQuotes"
 import quotes from "./quotes";
 
 const QuoteBox = ()=>{
     const quo = quotes;
+    const colors = Colors;
     const getRandom = ()=>{
         return Math.floor(Math.random() * (quo.quotes.length));
     }
+    const getColor =()=>{
+        return document.documentElement.style.setProperty('--main-color', colors[Math.floor(Math.random() * (colors.length))].hex);
+    }
     
     const [ quote, setQuote] = useState(quo.quotes[getRandom()]);
+    const [ color, setColor] = useState(getColor())
 
     const handleQuote=()=> {
         setQuote(quo.quotes[getRandom()])
+        setColor(getColor)    
     }
 
     return <div className="container">
